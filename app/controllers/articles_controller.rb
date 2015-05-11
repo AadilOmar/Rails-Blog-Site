a75df6633429dc8688f9653ab5ed6488b28d6284
aomar7 <aomar7@gatech.edu>
2015-05-11 12:07:19 -0400

    added comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,28 +1,34 @@
 class ArticlesController < ApplicationController
 
 	def new
+		puts "in new"
 		@article = Article.new
 	end
 
 	def show
+		puts "in show"
 		@article = Article.find(params[:id])
 	end	
 
 	def edit
+		puts 'in edit'	
 		@article = Article.find(params[:id])
 	end	
 
 	def index
+		puts 'in index'
 		@articles = Article.all
 	end
 
 	def destroy
+		puts 'in destroy'
 		@article = Article.find(params[:id])
 		@article.destroy
 		redirect_to articles_path
 	end
 
 	def update
+		puts 'in update'
 	  @article = Article.find(params[:id])
 	  if @article.update(article_params)
 	    redirect_to @article
@@ -32,6 +38,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
+		puts 'in create'
 		@article = Article.new(article_params)
 		#article.save returns the validation output. If passes, save. Else, do nothing
 		if @article.save
